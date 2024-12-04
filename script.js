7c70217696825080ae854004d802defa94e91204
kad5 <khaled.mohamed.1807@gmail.com>
2024-12-04 18:49:54 +0200

    fixed the bugs with wrting equations in succession. added comments.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+const display = document.getElementById("display");
+const container = document.getElementById("container");
+
 let input = "0";
 let value1 = 0;
 let value2 = 0;
 let op = null;
 let result = 0;
-
-const display = document.getElementById("display");
-const container = document.getElementById("container");
 
 function displayNum() {
   display.textContent = input; // makes the display show my input value. it is zero by default
@@ -19,10 +19,10 @@ function modifyInput(num) {
       input = input + ".";
       displayNum(); // adds the decimal while preserving the number before even if it was the default input 0
     }
-  } else if (input === "0") {
+  } else if (input == "0") {
     if (num === "0") {
       input = "0";
-      displayNum(); // if the user adds another zero, there should be no 00
+      displayNum(); // checks first if the input is the string zero or is 0 and prevents anymore 00
     } else {
       input = num;
       displayNum(); // if the user adds a number for example 5, there should be no 05
@@ -34,8 +34,8 @@ function modifyInput(num) {
 }
 
 function chooseSign() {
-  if (input === "0") {
-    return input === "0"; // checks first if the input is the default zero and denies adjusting the sign if so
+  if (input == "0") {
+    return input === "0"; // checks first if the input is the string zero or is 0 and denies adjusting the sign if so
   } else if (input[0] !== "-") {
     input = "-" + input;
     displayNum(); // if there is no - then add it to index 0
@@ -44,90 +44,59 @@ function chooseSign() {
     displayNum(); // if there is a - then remove it
   }
 }
-modifyInput(7);
-chooseOpSign("+");
-modifyInput(7);
-chooseOpSign("+");
-useEqualSign();
-chooseOpSign("+");
-chooseOpSign("+");
-chooseOpSign("+");
-useEqualSign();
-
-console.log(value1);
-
-function chooseOpSign(opSign) {
-  if (op) {
-    value1 = Number(input);
-    useEqualSign();
-  } else {
-    value1 = Number(input);
-    result = Number(input);
-    input = "0";
-    op = opSign;
-  }
-}
 
 function clearCalc() {
   input = "0";
   value1 = 0;
   value2 = 0;
-  result = 0;
   op = null;
-  display.textContent = 0;
-}
-
-function useEqualSign() {
-  value2 = Number(input);
-  operate();
-  value1 = 0;
-  value2 = 0;
-  input = result;
+  result = 0;
+  displayNum(); // resets everything to the starting condition
 }
 
 function operate() {
   switch (op) {
     case "+":
       result = value1 + value2;
-      display.textContent = result;
       break;
     case "-":
       result = value1 - value2;
-      display.textContent = result;
       break;
     case "/":
       result = value1 / value2;
-      display.textContent = result;
       break;
     case "*":
       result = value1 * value2;
-      display.textContent = result;
-      break;
+      break; //produce result based on v1 and v2
   }
 }
 
-/*
-modifyInput(1);
-modifyInput(1);
-chooseSign();
-chooseSign();
-chooseOpSign("-");
-modifyInput(5);
-operate();
-clearCalc();
-modifyInput(1);
-chooseOpSign("+");
-modifyInput(15);
-operate();
-console.log(value1);
+function useEqualSign() {
+  if (op === null) {
+    return; // this doenst calculate if the equal botton is clicked and we dont have an operator to act
+  }
+  if (input === "") {
+    value2 = Number(value1); // if to calculate while v2 is not enetered after choosing an operator, then set v2 = v1
+  } else {
+    value2 = Number(input); // if operator was chosen and input for v2 was added after
+  }
+  operate();
+  input = result; // set the input to the result value to be ready for input to be added to it or used as is
+  value1 = 0; // clean up
+  value2 = 0; // clean up
+  op = null; // make sure the operator has been deselected after completing the equation and closes the equation
+  displayNum();
+}
 
-modifyInput(7);
-chooseOpSign("*");
-modifyInput(9);
-chooseOpSign("-");
-modifyInput(11);
-chooseOpSign("+");
-modifyInput(10);
-operate();
-console.log(result);
-*/
+function chooseOpSign(opSign) {
+  if (op) {
+    useEqualSign(); // calculates if an operator is selected while an equation is already open
+    value1 = Number(input); // locks the v1 as the input (the input pooled from the result of the previous equation)
+    op = opSign; // opens a new equation with the new operator selected
+    input = ""; // clears the input variable ready for v2 handling
+  } else {
+    value1 = Number(input); // locks the v1 as the input
+    op = opSign; // opens a new equation and selects the operator sign
+    input = ""; // clears the input variable ready for v2 handling
+  }
+}
